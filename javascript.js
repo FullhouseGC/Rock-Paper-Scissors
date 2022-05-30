e7bec3803fc7624f9efaa521b3c3a16f01fcf993
FullhouseGC <jimmyvdb96@outlook.com>
2022-05-30 16:13:54 +0100

    Created function where computer chooses random item from array

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-console.log("test");
+let computerChoice = ["Piedra", "Papel", "Tijera"];
+function computerRandom(){
+    return computerChoice[Math.floor(Math.random()*computerChoice.length)]
+}
+console.log(computerRandom())
