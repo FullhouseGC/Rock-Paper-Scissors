466e517bde9b5c09a090a1c9a0db1f8a854a1f08
FullhouseGC <jimmyvdb96@outlook.com>
2022-05-30 18:56:05 +0100

    Added player choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
+// Array where the choices are stored for computer
 let computerChoice = ["Piedra", "Papel", "Tijera"];
+
+// This function will choose a random item from the array computerChoice 
 function computerRandom(){
     return computerChoice[Math.floor(Math.random()*computerChoice.length)]
 }
 console.log(computerRandom())
+
+// Promt asking for players choice
+let playerPrompt = prompt("Piedra, Papel o Tijera?");
+
+// Converting the answer in all caps so they can write it case insensitive
+let playerChoice = playerPrompt.toUpperCase();
